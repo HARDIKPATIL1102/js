84d065dec95bd4c35505ea48aa4fcb2e2ae82d49
HARDIKPATIL1102 <patilhardik082@gmail.com>
2026-04-30 15:29:44 +0000

    conversion of datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,36 @@
+let score=null
+//console.log(typeof score)
+let valueinnumber=Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+//33=>33
+//33abc=>naa
+//null=>0
+
+
+let isloggedin= undefined
+let booleanisloggedin=Boolean(isloggedin)
+//console.log(booleanisloggedin);
+//console.log(typeof booleanisloggedin);
+//1=>true
+//0=>false
+//hardik=>true
+//""=>false
+//7=>true
+//8=>true
+//null=>false
+//undefined=>false
+
+let number=undefined
+let stringnumber=String(number)
+console.log(typeof stringnumber);
+console.log(stringnumber);
+
+//30=>30
+//null=>null
+//undefined=>undefined
+
+
+
+

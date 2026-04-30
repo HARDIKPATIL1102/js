bf52eb011f4bd9a997e4bcfe96795b060c295b44
HARDIKPATIL1102 <patilhardik082@gmail.com>
2026-04-30 17:21:41 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+//console.log(2>1)
+//console.log(2<1)
+//console.log(2>=1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1);//this automatically convert string"2" into number
+
+
+//console.log(null >0);
+//console.log(null==0);
+//console.log(null>=0);
+//console.log(null<=0);
+//comparison convert null to a number treating it as 0. that why null>=0 and null<=0 is true.
+
+console.log(undefined ==0);
+console.log(undefined >=0);
+console.log(undefined <=0);
+console.log(undefined >0);
+console.log(undefined <0);
+//undefined give zero to all
+
+console.log("2"===2);
+//=== check strictly it check data types also that why false.

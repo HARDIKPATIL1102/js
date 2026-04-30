9fa5ef55da36059322470020fdf20d30b34c88a8
HARDIKPATIL1102 <patilhardik082@gmail.com>
2026-04-30 17:52:32 +0000

    dtsummary

---
+++ 01_basics/dtsummary.js
@@ -0,0 +1,9 @@
+//datatype of two type
+//1.non-primitive
+//2.primitive
+/*two type because their a concept called call by value and call by refrence.
+  how data types are stored and how u can access them is the difference between them.
+  on that basis data is catagorised.primitive data type r call by value and non primitive r 
+  called by reference.*/
+  //types of primitive:string,symbol,Bigint,number,null,undefined,boolean
+  //types of non-primitive :arrays,function,object

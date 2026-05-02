f87922dcd286951b8f2507a01317fcf8026348a3
HARDIKPATIL1102 <patilhardik082@gmail.com>
2026-05-02 15:15:14 +0000

    number and math

---
+++ 01_basics/numnmath.js
@@ -0,0 +1,30 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);//tostring()convert number into string.
+// console.log(balance.toFixed(2));
+
+const othernum = 123.4567
+// console.log(othernum.toPrecision(4));
+
+// console.log(Math);
+// console.log(Math.abs(-4));//it convert negative no. to positive no..
+// console.log(Math.ceil(4.5));//it give higher value.ex.4.4=5.
+// console.log(Math.floor(4.0));
+// console.log(Math.min(3,8,5,7));
+// console.log(Math.max(4,8,10,69));
+
+
+console.log(Math.random());//give random value between 0 and 1.
+console.log((Math.random()*10) + 1);//1 because it ensure the value will be between 1 to 9.
+console.log(Math.floor(Math.random()*10)+1);//floor give least vale like example 4.4=4.
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);//value will be 10 or greater.
+
+
+
